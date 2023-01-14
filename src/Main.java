684e48555a62b7685bcc5410e3b0540d8132c9d5
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-01-14 18:54:53 +0100

    ajout de la creation d'une fenetre

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import affichage.Ecran;
+
 public class Main {
     public static void main(String[] args) {
+        Ecran ecran = new Ecran();
 
     }
 }
--- src/affichage/Ecran.java
+++ src/affichage/Ecran.java
@@ -1,4 +1,28 @@
 package affichage;
 
-public class Ecran {
+import javax.swing.*;
+
+public class Ecran{
+    private final JFrame fenetre;
+
+    /**
+     * constructeur de la classe Ecran
+     * @param titre titre que l'on donne à la fenetre
+     */
+    public Ecran(String titre) {
+        fenetre = new JFrame(titre);
+        fenetre.setSize(1000,1000);
+        fenetre.setVisible(true);
+    }
+
+    public Ecran(){
+        this("DICTIONNAIRE");
+    }
+
+    /**
+     * méthode qui ferme la fenêtre
+     */
+    public void close(){
+        fenetre.setDefaultCloseOperation(3);
+    }
 }
