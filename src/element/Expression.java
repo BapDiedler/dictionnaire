e869033282282e7a6fa9e924ce53f9a77f35a019
DIEDLER Baptiste <baptiste.diedler3@etu.univ-lorraine.fr>
2023-01-14 18:10:21 +0100

    ajout d'un diagramme et mise en place de la classe Expression

--- src/element/Expression.java
+++ src/element/Expression.java
@@ -1,4 +1,56 @@
 package element;
 
 public class Expression {
+    private String eleFrancais;
+    private String eleAnglais;
+
+    /**
+     * constructeur de la classe Expression qui fait la relation entre un mot francais et anglais
+     * @param eleFrancais le mot en francais
+     * @param eleAnglais le mot en anglais
+     */
+    public Expression(String eleFrancais, String eleAnglais){
+        this.eleFrancais = eleFrancais;
+        this.eleAnglais = eleAnglais;
+    }
+
+    /**
+     * constructeur qui construit un mot sans sa traduction
+     * @param eleFrancais mot que l'on ajoute
+     */
+    public Expression(String eleFrancais){
+        this(eleFrancais,null);
+    }
+
+    /**
+     * setter sur le mot en francais
+     * @param eleFrancais le mot que l'on donne
+     */
+    public void setFrancais(String eleFrancais){
+        this.eleFrancais = eleFrancais;
+    }
+
+    /**
+     * setter sur le mot en anglais
+     * @param eleAnglais le mot que l'on donne
+     */
+    public void setAnglais(String eleAnglais){
+        this.eleAnglais = eleAnglais;
+    }
+
+    /**
+     * getter sur le mot francais
+     * @return le mot francais
+     */
+    public String getFrancais(){
+        return eleFrancais;
+    }
+
+    /**
+     * getter sur le mot anglais
+     * @return le mot anglais
+     */
+    public String getAnglais(){
+        return eleAnglais;
+    }
 }
